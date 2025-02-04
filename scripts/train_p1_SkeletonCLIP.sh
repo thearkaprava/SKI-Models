e5606e6f2e01fd831490330a759e5d225b2f9001
thearkaprava <asinha13@uncc.edu>
2025-02-04 12:59:01 -0500

    Updated config

--- scripts/train_p1_SkeletonCLIP.sh
+++ scripts/train_p1_SkeletonCLIP.sh
@@ -1,12 +1,12 @@
 d#!/bin/bash
 
 # Set environment variables if needed
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 # Run the distributed training
 python -m torch.distributed.launch \
     --master_port 29501 \
-    --nproc_per_node=4 \
+    --nproc_per_node=8 \
     P1_SkelCLIP_main.py \
     -cfg /PATH/TO/CONFIG \
     --output /PATH/TO/Phase1_OUTPUT/ \
--- scripts/train_p2_SKIViFiCLIP.sh
+++ scripts/train_p2_SKIViFiCLIP.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Set environment variables if needed
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 # Run the distributed training
 python -m torch.distributed.launch \
     --master_port 29501 \
-    --nproc_per_node=4 \
+    --nproc_per_node=8 \
     P2_SKIVLM_main.py \
     -cfg /PATH/TO/CONFIG \
     --output /PATH/TO/Phase2_OUTPUT/ \
